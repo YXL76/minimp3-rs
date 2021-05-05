4723c902045ff31771a1aca44b46f9fd24cbc985
YXL <chenxin.lan.76@gmail.com>
2021-05-06 00:52:39 +0800

    no simd for aarch64-windows

--- minimp3-sys/build.rs
+++ minimp3-sys/build.rs
@@ -1,9 +1,23 @@
 extern crate cc;
 
+// #[cfg(all(target_os = "windows", target_arch = "aarch64"))]
 fn main() {
-    cc::Build::new()
-        .include("minimp3/")
-        .file("minimp3.c")
-        .define("MINIMP3_IMPLEMENTATION", None)
-        .compile("minimp3");
+    if std::env::var("CARGO_CFG_TARGET_OS").map_or(false, |os| os == "windows")
+        && std::env::var("CARGO_CFG_TARGET_ARCH").map_or(false, |arch| arch == "aarch64")
+    {
+        cc::Build::new()
+            .include("minimp3/")
+            .file("minimp3.c")
+            .define("MINIMP3_IMPLEMENTATION", None)
+            .define("MINIMP3_ONLY_MP3", None)
+            .define("MINIMP3_NO_SIMD", None)
+            .compile("minimp3");
+    } else {
+        cc::Build::new()
+            .include("minimp3/")
+            .file("minimp3.c")
+            .define("MINIMP3_IMPLEMENTATION", None)
+            .define("MINIMP3_ONLY_MP3", None)
+            .compile("minimp3");
+    }
 }
